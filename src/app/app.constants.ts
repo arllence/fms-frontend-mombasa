73ec5501f0fa592ff7e2b37011e7a6b44ac85eed
bobkings <bobiekings@gmail.com>
2024-04-03 13:02:01 +0300

    updates

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -32,6 +32,9 @@ export let department_detail_url = serverurl + API_VERSION + 'department/departm
 export let department_url = serverurl + API_VERSION + 'department/department';
 export let upload_departments_url = serverurl + API_VERSION + 'department/upload';
 
+// department
+export let slt_url = serverurl + API_VERSION + 'slt/slt';
+
 
 // mms
 export let quote_url = serverurl + API_VERSION + 'mms/quote';
