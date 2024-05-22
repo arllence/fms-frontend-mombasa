4b7911fd4de468b57f5358c3d21cf6b4f52ecf24
bobkings <bobiekings@gmail.com>
2024-05-22 12:12:33 +0300

    cleanups

--- src/app/reports/config/reports.module.ts
+++ src/app/reports/config/reports.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { CommonSharedModule } from '../../common-module/common-module/common-module.module';
 import { MainReportComponent } from '../container/requests/main.component';
 import { ReplacementsReportComponent } from '../container/transport/main.component';
-import { FlightReportComponent } from '../container/flights/main.component';
-import { JourneyReportComponent } from '../container/journeys/main.component';
 import { ReportsRoutingModule } from './reports-routing.module';
 
 
@@ -13,8 +11,6 @@ import { ReportsRoutingModule } from './reports-routing.module';
   declarations: [
     MainReportComponent,
     ReplacementsReportComponent,
-    FlightReportComponent,
-    JourneyReportComponent
   ],
   imports: [
     CommonSharedModule,
