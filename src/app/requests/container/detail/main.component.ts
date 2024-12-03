de976a5f908cd830afec5a01008e3118eefe126b
bobkings <bobiekings@gmail.com>
2024-12-03 12:26:20 +0300

    feature: close status update

--- src/app/requests/container/detail/main.component.ts
+++ src/app/requests/container/detail/main.component.ts
@@ -31,7 +31,7 @@ export class DetailRequestComponent implements OnInit {
   public createRecordForm: FormGroup;
   public noteForm: FormGroup;
   public assignForm: FormGroup;
-  public hiredForm: FormGroup;
+  public closeForm: FormGroup;
   public ReplacementForm: FormGroup;
   public BudgetApprovalForm: FormGroup;
   validation_messages: any;
@@ -51,12 +51,8 @@ export class DetailRequestComponent implements OnInit {
   dtTrigger: Subject<any> = new Subject<any>();
   @ViewChild('createModal') public createModal: ModalDirective;
   @ViewChild('noteModal') public noteModal: ModalDirective;
-  @ViewChild('hiredModal') public hiredModal: ModalDirective;
-  @ViewChild('EditHiredModal') public EditHiredModal: ModalDirective;
-  @ViewChild('approveModal') public approveModal: ModalDirective;
+  @ViewChild('closeModal') public closeModal: ModalDirective;
   @ViewChild('assignModal') public assignModal: ModalDirective;
-  @ViewChild('budgetApprovalModal') public budgetApprovalModal: ModalDirective;
-  @ViewChild('approveRequestModal') public approveRequestModal: ModalDirective;
   records: any = [];
   searchString: string;
   previous: string | null;
@@ -95,29 +91,12 @@ export class DetailRequestComponent implements OnInit {
       comments: new FormControl('', Validators.compose([Validators.required]))
     });
 
-    this.hiredForm = this.formBuilder.group({
-      // recruit_id: new FormControl('', Validators.compose([Validators.required])),
-      // status: new FormControl('', Validators.compose([Validators.required])),
-      name: new FormControl('', Validators.compose([Validators.required])),
-      email: new FormControl('', Validators.compose([Validators.required])),
-      employee_no: new FormControl('', Validators.compose([Validators.required])),
-      reporting_date: new FormControl('', Validators.compose([Validators.required])),
-      reporting_station: new FormControl('', Validators.compose([Validators.required])),
-      working_station: new FormControl('',),
+    this.closeForm = this.formBuilder.group({
+      request_id: new FormControl('', Validators.compose([Validators.required])),
+      comments: new FormControl('', ),
     });
 
-    
-
-    this.BudgetApprovalForm = this.formBuilder.group({
-      recruit_id: new FormControl('', Validators.compose([Validators.required]))
-    });
-
-    this.ReplacementForm = this.formBuilder.group({
-      recruit_id: new FormControl('', Validators.compose([Validators.required])),
-      name: new FormControl('', Validators.compose([Validators.required])),
-      position_number: new FormControl('', Validators.compose([Validators.required])),
-      date_of_leaving: new FormControl('', Validators.compose([Validators.required])),
-    });
+  
 
     let request_id = this.route.snapshot.paramMap.get('id');
     if (request_id){
@@ -162,50 +141,15 @@ export class DetailRequestComponent implements OnInit {
     this.noteModal.show()
   }
 
+  // set_update_request_status(status:any,request_id:any){
+  //   this.noteForm.patchValue({"recruit_id":request_id, "status":status});
+  //   this.noteModal.show();
+  // }
 
-
-
-
-
-  set_request_id(request_id:any){
-    this.BudgetApprovalForm.patchValue({"recruit_id":request_id});
+  set_close_request_status(request_id:any){
+    this.closeForm.patchValue({"request_id":request_id});
+    this.closeModal.show();
   }
-
-  set_update_request_status(status:any,request_id:any){
-    this.noteForm.patchValue({"recruit_id":request_id, "status":status});
-    this.noteModal.show();
-  }
-  set_update_hired_status(request_id:any){
-    this.hiredForm.patchValue({"recruit_id":request_id, "status":"HIRED"});
-    this.hiredModal.show();
-  }
-  set_employee_id(index:any){
-    const employee = this.records?.employees[index]
-    this.employee_id = employee?.id;
-    this.hiredForm.patchValue(employee)
-    this.EditHiredModal.show();
-  }
-
-  add_employees(){
-    if (this.hiredForm.valid){
-      const employee = this.hiredForm.value;
-      this.employees.push(employee);
-      this.hiredForm.reset()
-    } else {
-      this.administrationService.markFormAsDirty(this.hiredForm);
-    }
-  }
-
-  remove_employee(index:any){
-    this.employees.splice(index, 1);
-  }
-
-  edit_employee(index:any){
-    const employee = this.employees[index];
-    this.hiredForm.patchValue(employee)
-    this.employees.splice(index, 1);
-  }
-
 
 
   fetchRecords(request_id:any) {
@@ -287,7 +231,6 @@ export class DetailRequestComponent implements OnInit {
   }
  
   assign() {
-
     if (this.assignForm.valid) {
 
       let payload = this.assignForm.value
@@ -318,7 +261,6 @@ export class DetailRequestComponent implements OnInit {
   }
 
   add_note() {
-
     if (this.noteForm.valid) {
 
       const payload = this.noteForm.value
@@ -348,28 +290,22 @@ export class DetailRequestComponent implements OnInit {
     }
   }
 
-  update_hired_status() {
+  close_incident() {
 
-    if (this.hiredForm.valid) {
-      this.add_employees()
-    }
-
-    const payload = {
-      'recruit_id' : this.request_id,
-      'employees' : this.employees,
-    }
+    const payload = this.closeForm.value
 
     this.sweetalertService.showConfirmation('Confirmation',
-    'Do you wish to proceed marking requisition as Hired?').then((res) => {
+    'Do you wish to proceed marking incident as Closed?').then((res) => {
       if (res) {
         this.loadingService.showloading();
-        this.administrationService.postrecord(hired_url, payload).subscribe((res) => {
+        this.administrationService.patchrecord(incident_url, payload).subscribe((res) => {
           if (res) {
             this.loadingService.hideloading();
-            this.hiredForm.reset();
-            this.sweetalertService.showAlert('Success', 'Requisition Updated Successfully', 'success');
+            this.sweetalertService.showAlert('Success', 'Closed Successfully', 'success');
             this.fetchRecords(this.request_id);
-            this.hiredModal.hide()
+            this.fetchNotes();
+            this.closeForm.reset();
+            this.closeModal.hide();
           } else {
             this.loadingService.hideloading();
           }
@@ -378,70 +314,6 @@ export class DetailRequestComponent implements OnInit {
     });
   }
 
-  edit_hired_status() {
-
-    if (!this.hiredForm.valid) {
-      this.administrationService.markFormAsDirty(this.hiredForm);
-      return
-    }
-
-    let payload = this.hiredForm.value
-
-    payload['request_id'] = this.employee_id
-
-    this.sweetalertService.showConfirmation('Confirmation',
-    'Do you wish to proceed editing record?').then((res) => {
-      if (res) {
-        this.loadingService.showloading();
-        this.administrationService.updaterecord(hired_url, payload).subscribe((res) => {
-          if (res) {
-            this.loadingService.hideloading();
-            this.hiredForm.reset();
-            this.sweetalertService.showAlert('Success', 'Requisition Updated Successfully', 'success');
-            this.fetchRecords(this.request_id);
-            this.EditHiredModal.hide()
-          } else {
-            this.loadingService.hideloading();
-          }
-        });
-      }
-    });
-  }
-
-  upload_budget_approval() {
-
-    if (this.BudgetApprovalForm.valid) {
-
-      const payload = this.BudgetApprovalForm.value
-
-      this.formData.append('budget_approval', this.fileData);
-      this.formData.append('payload', JSON.stringify(payload));
-     
-      this.sweetalertService.showConfirmation('Confirmation',
-      'Do you wish to proceed uploading budget?').then((res) => {
-        if (res) {
-          this.loadingService.showloading();
-            this.administrationService.postrecord(upload_budget_approval_url, this.formData).subscribe((res) => {
-              if (res) {
-                this.loadingService.hideloading();
-                this.BudgetApprovalForm.reset();
-                this.sweetalertService.showAlert('Success', 'Uploaded Successfully', 'success');
-                this.fetchRecords(this.request_id);
-                this.budgetApprovalModal.hide()
-
-              } else {
-                this.loadingService.hideloading();
-              }
-            });
-          }
-        });
-
-    } else {
-      this.toastService.showToastNotification('error', 'Omitted Fields Required ', 'Error');
-      this.administrationService.markFormAsDirty(this.BudgetApprovalForm);
-
-    }
-  }
 
 
 }
